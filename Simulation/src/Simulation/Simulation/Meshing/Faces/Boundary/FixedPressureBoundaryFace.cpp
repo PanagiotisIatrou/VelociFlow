92366bb656a2291e947b929c9875089f9d74b083
Panagiotis Iatrou <iatrou.panag@gmail.com>
2025-01-07 18:10:49 +0200

    Small reformating

--- Simulation/src/Simulation/Simulation/Meshing/Faces/Boundary/FixedPressureBoundaryFace.cpp
+++ Simulation/src/Simulation/Simulation/Meshing/Faces/Boundary/FixedPressureBoundaryFace.cpp
@@ -4,28 +4,29 @@
 
 #include "../../Nodes/Node.hpp"
 
-FixedPressureBoundaryFace::FixedPressureBoundaryFace(const double pressure, const Orientation orientation) : BoundaryFace(BoundaryType::FixedPressure, orientation) {
+FixedPressureBoundaryFace::FixedPressureBoundaryFace(const double pressure, const Orientation orientation)
+    : BoundaryFace(BoundaryType::FixedPressure, orientation) {
     m_pressure = pressure;
 }
 
 double FixedPressureBoundaryFace::get_field_value(const Field field) const {
     switch (field) {
-        case Field::VelocityX : {
+        case Field::VelocityX: {
             return m_node_neighbour->get_field_value(Field::VelocityX);
         }
-        case Field::VelocityY : {
+        case Field::VelocityY: {
             return m_node_neighbour->get_field_value(Field::VelocityY);
         }
-        case Field::Pressure : {
+        case Field::Pressure: {
             return m_pressure;
         }
-        case Field::PressureCorrection : {
+        case Field::PressureCorrection: {
             return 0.0;
         }
-        case Field::Dye : {
+        case Field::Dye: {
             return m_node_neighbour->get_field_value(Field::Dye);
         }
-        default : {
+        default: {
             std::cerr << std::endl << "Field not recognised" << std::endl;
             exit(1);
         }
