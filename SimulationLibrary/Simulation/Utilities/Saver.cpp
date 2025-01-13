0b073c5e4f5b06724554b468e2a79ab62aa0e311
Panagiotis Iatrou <iatrou.panag@gmail.com>
2025-01-13 23:30:00 +0200

    Increase the digits of double numbers on the output file to 15 digits

--- SimulationLibrary/Simulation/Utilities/Saver.cpp
+++ SimulationLibrary/Simulation/Utilities/Saver.cpp
@@ -27,7 +27,7 @@ void Saver::write_simulation_name(const SimulationName simulation_name) const {
 
 void Saver::write_domain_size(const double size_x, const double size_y) const {
     fprintf(m_file, "[domain_size]\n");
-    fprintf(m_file, "%f,%f\n", size_x, size_y);
+    fprintf(m_file, "%.15f,%.15f\n", size_x, size_y);
 }
 
 void Saver::write_grid_size(const int size_x, const int size_y) const {
@@ -42,22 +42,22 @@ void Saver::write_execution_time(const double execution_time) const {
 
 void Saver::write_dt(const double dt) const {
     fprintf(m_file, "[dt]\n");
-    fprintf(m_file, "%f\n", dt);
+    fprintf(m_file, "%.15f\n", dt);
 }
 
 void Saver::write_density(const double density) const {
     fprintf(m_file, "[density]\n");
-    fprintf(m_file, "%f\n", density);
+    fprintf(m_file, "%.15f\n", density);
 }
 
 void Saver::write_viscosity(const double viscosity) const {
     fprintf(m_file, "[viscosity]\n");
-    fprintf(m_file, "%f\n", viscosity);
+    fprintf(m_file, "%.15f\n", viscosity);
 }
 
 void Saver::write_tolerance(const EquationType equation_type, const double value) const {
     fprintf(m_file, "[tolerance]\n");
-    fprintf(m_file, "%s,%f\n", equation_type_to_str(equation_type).c_str(), value);
+    fprintf(m_file, "%s,%.15f\n", equation_type_to_str(equation_type).c_str(), value);
 }
 
 void Saver::write_timesteps_count(const int timesteps) const {
@@ -99,7 +99,7 @@ void Saver::write_field(const Field field) const {
             const Node *node_P = m_mesh->get_node(i, j);
             if (node_P != nullptr) {
                 const double value = node_P->get_field_value(field);
-                fprintf(m_file, "%d,%d,%f\n", i, j, value);
+                fprintf(m_file, "%d,%d,%.15f\n", i, j, value);
             } else {
                 fprintf(m_file, "%d,%d,-\n", i, j);
             }
@@ -109,12 +109,12 @@ void Saver::write_field(const Field field) const {
 
 void Saver::write_normalization_values(const EquationType equation_type, Equation *equation) const {
     fprintf(m_file, "[normalization]\n");
-    fprintf(m_file, "%s,%f", equation_type_to_str(equation_type).c_str(), equation->get_imbalance_normalization_factor());
+    fprintf(m_file, "%s,%.15f", equation_type_to_str(equation_type).c_str(), equation->get_imbalance_normalization_factor());
 
     // Extra normalization value only for the pressure correction equation
     // (mass imbalance)
     if (equation_type == EquationType::PressureCorrection) {
-        fprintf(m_file, ",%f", static_cast<PressureCorrection *>(equation)->get_mass_imbalance_normalization_factor());
+        fprintf(m_file, ",%.15f", static_cast<PressureCorrection *>(equation)->get_mass_imbalance_normalization_factor());
     }
 
     fprintf(m_file, "\n");
