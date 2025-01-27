020358160dea4e09a4536b95beea1f6719c20fb6
Panagiotis Iatrou <iatrou.panag@gmail.com>
2025-01-27 20:24:23 +0200

    Refactored density in Time term

--- SimulationLibrary/Simulation/Equations/Equations/TermDiscretizations/Time/Time.cpp
+++ SimulationLibrary/Simulation/Equations/Equations/TermDiscretizations/Time/Time.cpp
@@ -10,13 +10,14 @@ Time::Time(Node *node, const Field field, const bool include_density) : TermDisc
 Coefficients Time::get_effects() const {
     Coefficients coefficients;
 
-    double extra = m_node->get_dx() * m_node->get_dy();
-    if (m_include_density) {
-        extra *= m_node->get_density();
-    }
-
+    const double extra = m_node->get_dx() * m_node->get_dy();
     coefficients.center += extra;
     coefficients.source += extra * m_node->get_previous_timestep_field_value(m_field);
+
+    // Multiply by density if needed
+    if (m_include_density) {
+        coefficients *= m_node->get_density();
+    }
 
     return coefficients;
 }
