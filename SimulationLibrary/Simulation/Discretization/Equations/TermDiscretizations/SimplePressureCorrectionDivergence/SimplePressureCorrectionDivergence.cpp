85d56ac257f390e45d463e01e25ae03cf90d91f9
Panagiotis Iatrou <iatrou.panag@gmail.com>
2025-04-08 15:53:08 +0300

    Added comments

--- SimulationLibrary/Simulation/Discretization/Equations/TermDiscretizations/SimplePressureCorrectionDivergence/SimplePressureCorrectionDivergence.cpp
+++ SimulationLibrary/Simulation/Discretization/Equations/TermDiscretizations/SimplePressureCorrectionDivergence/SimplePressureCorrectionDivergence.cpp
@@ -30,15 +30,15 @@ Coefficients SimplePressureCorrectionDivergence::get_effects() const {
             velocity_face = interior_face_face->get_velocity();
         }
 
+        // Add the source term
         const double area = orientation == Orientation::Horizontal ? m_node->get_dy() : m_node->get_dx();
         const double sign = direction == Direction::West || direction == Direction::South ? 1.0 : -1.0;
-
         direction_coefficients.source += sign * velocity_face * area;
 
+        // Add the direction effects
         const EquationType equation_type =
             orientation == Orientation::Horizontal ? EquationType::MomentumX : EquationType::MomentumY;
         const double momentum_a_P = m_node->get_equation_coefficient(equation_type, CoefficientType::Center);
-
         if (face_direction->get_face_type() != FaceType::Boundary) {
             const double momentum_a_direction = m_node->get_neighbouring_node(direction)->get_equation_coefficient(
                 equation_type, CoefficientType::Center);
